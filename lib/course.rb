cb71ac3031cee34f69d168939437b202de073540
Damely Tineo <damely.tineo07@gmail.com>
2020-02-05 14:17:40 +0000

    Automatically backed up by Learn

--- lib/course.rb
+++ lib/course.rb
@@ -6,14 +6,6 @@ class Course
   def initialize
     @@all << self
   end
-  
-  # def assign_values(values)
-  #   values.each_key do |k, v|
-  #     # How send method would look a like
-  #     # self.name = value[k]
-  #     self.send("#{k}=", values[k])
-  #   end
-  # end
 
   def self.all 
     @@all
